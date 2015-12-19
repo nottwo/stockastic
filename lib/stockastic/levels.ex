6c55935936ab0b3e0da57a265ad1c63520e284e2
Kirill Pimenov <kirill@pimenov.cc>
2015-12-19 19:39:20 +0100

    Documented GM API

--- lib/stockastic/levels.ex
+++ lib/stockastic/levels.ex
@@ -1,26 +1,97 @@
 defmodule Stockastic.Levels do
   import Stockastic
 
-  def start(level, config) do
-    post "gm/levels/#{level}", config
+  @moduledoc """
+  This module exposes gm API, as described here:
+  https://discuss.starfighters.io/t/the-gm-api-how-to-start-stop-restart-resume-trading-levels-automagically/143
+  """
+
+  @doc """
+  Starts a level by its name
+
+  ## Example
+
+      Stockastic.Levels.start "first_steps", client
+
+  """
+  @spec start(binary, Client.t) :: Stockastic.response
+  def start(level, client) do
+    post "gm/levels/#{level}", client
+  end
+
+  @doc """
+  NOT YET IMPLEMENTED ON SERVER, as of this writing
+
+  List accessible levels, with their level ids
+
+  ## Example
+
+      Stockastic.Levels.list client
+
+  """
+  @spec list(Client.t) :: Stockastic.response
+  def list(client) do
+    get "gm/levels", client
   end
 
   defmodule Instances do
 
-    def status(instance_id, config) do
-      get "gm/instances/#{instance_id}", config
+    @moduledoc """
+    This module implements level instances manipulation API, as specified on
+    https://discuss.starfighters.io/t/the-gm-api-how-to-start-stop-restart-resume-trading
+    """
+
+    @doc """
+    Get the status (open/closed, tradingDay, etc) of the target level instance
+
+    ## Example
+
+        Stockastic.Levels.Instances.status 117261, client
+
+    """
+    @spec status(integer, Client.t) :: Stockastic.response
+    def status(instance_id, client) do
+      get "gm/instances/#{instance_id}", client
     end
 
-    def restart(instance_id, config) do
-      post "gm/instances/#{instance_id}/restart", config
+    @doc """
+    Restart given level instance
+
+    ## Example
+
+        Stockastic.Levels.Instances.restart 117261, client
+
+    """
+    @spec restart(integer, Client.t) :: Stockastic.response
+    def restart(instance_id, client) do
+      post "gm/instances/#{instance_id}/restart", client
     end
 
-    def stop(instance_id, config) do
-      post "gm/instances/#{instance_id}/stop", config
+    @doc """
+    Stop given level instance
+
+    ## Example
+
+        Stockastic.Levels.Instances.restart 117261, client
+
+    """
+    @spec stop(integer, Client.t) :: Stockastic.response
+    def stop(instance_id, client) do
+      post "gm/instances/#{instance_id}/stop", client
     end
 
-    def resume(instance_id, config) do
-      post "gm/instances/#{instance_id}/resume", config
+    @doc """
+    Resume given level instance
+    (helpful to remind you trading account id or venues and tickers for the current puzzle)
+
+    ## Example
+
+        Stockastic.Levels.Instances.restart 117261, client
+
+    """
+    @spec resume(integer, Client.t) :: Stockastic.response
+    def resume(instance_id, client) do
+      post "gm/instances/#{instance_id}/resume", client
     end
 
   end
